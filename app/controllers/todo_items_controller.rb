2d70810e75413526afdf84e2653ce1260ed0858f
Alex Cevallos <cevalloa@gmail.com>
2014-12-12 17:04:10 -0800

    Fixed RSpec Issue with spec/features/todo_items/create_spec.rb

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -10,12 +10,12 @@ class TodoItemsController < ApplicationController
 
   def create
   	@todo_list = TodoList.find(params[:todo_list_id])
-  	@todo_item = @todo_list.todo_items.new(todo_items_params)
+  	@todo_item = @todo_list.todo_items.new(todo_item_params)
   	if @todo_item.save
-  		flash[:success] = "Added todo list item"
+  		flash[:success] = "Added todo list item."
   		redirect_to todo_list_todo_items_path
   	else
-  		flash[:error] = "There was a problem adding that todo list item"
+  		flash[:error] = "There was a problem adding that todo list item."
   		render action: :new
   	end
   end
