2eec186cf2598aec3df6552b938716896ddbf90a
Alex Cevallos <cevalloa@gmail.com>
2014-12-14 00:24:13 -0800

    Added ability to mark item as complete

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -42,8 +42,14 @@ class TodoItemsController < ApplicationController
     else
       flash[:error] = "Todo list item could not be deleted."
     end
-    redirect_to todo_list_todo_items_path
+    redirect_to todo_list_todo_items_path, notice: "Todo item marked as completes"
 
+  end
+
+  def complete
+        @todo_item = @todo_list.todo_items.find(params[:id])
+        @todo_item.update_attribute(:completed_at, Time.now)
+        redirect_to todo_list_todo_items_path, notice: "Todo item markred as complete"
   end
 
   def url_options
